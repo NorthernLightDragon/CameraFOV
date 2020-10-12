026dc996bef66cedd8af56c6b2b38b61e5576521
AzureNorthernLight <42276346+NorthernLightDragon@users.noreply.github.com>
2020-10-12 19:49:08 +0200

    Add files via upload

---
+++ CameraFOV/CameraFOV.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFOV : MonoBehaviour
+{
+
+
+    // Limits
+    float startFOV = 15;        //starting
+    float minFOV = 10f;         //min zoom limit
+    float maxFOV = 20f;         //max zoom limit
+    float zoomSpeed = 3f;       //zoom speed
+    Camera cam;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+        cam.fieldOfView = startFOV;
+    }
+
+    void LateUpdate()
+    {             
+        // Zoom (wheel)
+        cam.fieldOfView = Mathf.Clamp(cam.fieldOfView - Input.mouseScrollDelta.y * zoomSpeed, minFOV, maxFOV);
+    }
+}
